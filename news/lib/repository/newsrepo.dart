09b5ee53fc2c753c328b5057f50a57b4f7419677
Jawwad Ahmed <mjawwadahmed2021@gmail.com>
2024-10-01 00:09:59 +0500

    updated

--- news/lib/repository/newsrepo.dart
+++ news/lib/repository/newsrepo.dart
@@ -6,7 +6,7 @@ class NewsRepo {
   // ignore: non_constant_identifier_names
   Future<newsheadlinesmodel> FetchNews(String source) async {
     var url =
-        'https://newsapi.org/v2/top-headlines?sources=$source&apiKey=f3361b080e00465fa3fb8c29bb53ea5f';
+        'https://newsapi.org/v2/top-headlines?sources=$source&apiKey=ur api key';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       var body = jsonDecode(response.body);
